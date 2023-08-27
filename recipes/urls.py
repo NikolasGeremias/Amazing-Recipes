36169f99e299521f7354474d3e42f003fe1e8e2a
Nikolas Geremias <nikolasgeremias27@gmail.com>
2023-08-27 11:11:39 -0300

    removed contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home 1')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/home.html')
